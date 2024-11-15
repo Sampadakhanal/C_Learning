bbfd5da40c1e03e4a90732609346a7dc92f6f2d2
Sampada Khanal <sampadakhanal84@gmail.com>
2024-11-15 15:29:14 +0545

    added whole code

--- hello.c
+++ hello.c
@@ -2,5 +2,14 @@ include<stdio.h>
 include<conio.h>
 
 int main(){
+    int a,b;
+    printf("Enter the first number:");
+    scanf("%n",&a);
+    printf("Enter the first number:");
+    scanf("%n",&b); 
     
+    int sum = a+b;
+    prinf("Sum of the two numbers is: ",sum);
+
+    return 0;
 }
