959ca2253debc43d18e7dca1c53cb0384af237fc
Sampada Khanal <sampadakhanal84@gmail.com>
2024-11-24 22:18:33 +0545

    Add some formula

---
+++ area.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main (){
+    float l,a;
+    printf("\n Enter length:");
+    scanf("%d",&l);
+    a = l*l;
+    printf("\n Area=%d",a);
+    return 0;
+}
