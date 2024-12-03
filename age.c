7ada99cc144f7ea59dc5fb8dc0f12480519a54b5
Sampada Khanal <sampadakhanal84@gmail.com>
2024-12-03 22:19:11 +0545

    Age

---
+++ age.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+Int main (){
+    int age;
+    Printf("Enter a age");
+    scanf("%d",&age);
+
+    if (age >=18){
+        printf("\n The person are eligible to vote");
+    }else{
+        printf("\n The person are not eligible to vote");
+    }
+    return 0;
+}
