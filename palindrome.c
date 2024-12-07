daf3fa7584de1656a92691d23eefa43c25194e2b
Sampada Khanal <sampadakhanal84@gmail.com>
2024-12-07 22:19:12 +0545

    number is palindrome or not

---
+++ palindrome.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main (){
+    int number,remainder,original,reversed=0;
+    printf("Enter a number:");
+    scanf("%d",&number);
+    original=number;
+    while(number=0)
+    {
+        remainder=number%10;
+        reversed=reversed*10+remainder
+        number=number/10;
+    }
+    if(original==reversed){
+        printf("\nThe number is a palindrome:");
+    }
+    else
+    {
+        printf("\n The number is not a palindrome:");
+    }
+    return 0;
+}
